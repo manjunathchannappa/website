2be116cf908c8906975de147a5a53bb85636a3f8
manjunathchannappa <personal.manjunathc@gmail.com>
2026-06-25 15:00:03 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('open');
-}
-
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', () => document.getElementById('nav').classList.remove('open'));
-});
